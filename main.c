f812e5d2f53491e6a1f13e3bcee916d78f7f6713
Leon <OfficialMuffin@protonmail.com>
2018-09-11 00:34:09 +0100

    Updated Files
    (See Changelog)

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #define BUILD_VERSION "             0.3"
 #define STEAM_VERSION "N3XT 1.57"
 #define AUTHOR "by OfficialMuffin"
+#define FLORA "Marrow Bulb, Cactus Flesh, Gamma Root, Solanium"
 
 //Notices your OS, OwO What's This
 #ifdef _WIN32
@@ -25,18 +26,20 @@ void introduction() {
 }
 
 void elements() {
-    const char *elements[4];
+    const char *elements[6];
     int i;
 
     elements[0] = "a) Carbon";
     elements[1] = "b) Ferrite Dust";
     elements[2] = "c) Copper";
-    elements[3] = "d) Di-Hydrogen (NOT AVALIABLE YET)";
+    elements[3] = "d) Oxygen";
+    elements[4] = "e) Paraffinium";
+    elements[5] = "f) Any Flora (Cactus Flesh, Gamma Root, Solanium etc..)";
 
-    printf("Here are the elements you are able to use\n");
+    printf("Here are the elements you are able to use:\n");
 
     //Print all elements in the array
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 6; i++) {
         printf("%s\n", elements[i]);
     }
 }
@@ -85,17 +88,40 @@ int main()
             }
     }
      if (choice == 'd') {
-        printf("How much Di-Hydrogen do you want to place into the refiner?");
+        printf("How much Oxygen do you want to place into the refiner?");
         printf("(You MUST enter an even number!): ");
         scanf("%d", &amount);
             if(amount & 0x1) {
-                printf("You are unable to process any more Ferrite Dust");
+                printf("You are unable to process any more Oxygen");
             }
             else {
-                    //Recipe 30:1
-                printf("You will have %d Di-Hydrogen Jelly\n", amount);
+                printf("You will have %d Carbon\n", amount);
             }
     }
+    if (choice == 'e') {
+        printf("How much Paraffinium do you want to place into the refiner?");
+        printf("(You MUST enter an even number!): ");
+        scanf("%d", &amount);
+            if(amount & 0x1) {
+                printf("You are unable to process any more Paraffinium");
+            }
+            else {
+                printf("You will have %d Ferrite Dust\n", amount);
+            }
+    }
+    if (choice == 'f') {
+        printf("How much Flora do you want to place into the refiner?\n");
+        printf("(Examples of Flora are %s): ", FLORA);
+        scanf("%d", &amount);
+            if(amount > 0 || amount <= 250) {
+                printf("You will have %d Carbon\n", amount);
+            }
+            else if (amount < 0 || amount > 250){
+                printf("You are unable to process any more Flora");
+
+            }
+    }
+
 
     //Keep this here!
     return 0;
